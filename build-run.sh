6945501db75cdef628bf76f39ca71343df0dc862
Ignacio Astaburuaga <ignaciochg@hotmail.com>
2020-09-29 23:34:42 -0700

    ass autoremoval

--- build-run.sh
+++ build-run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-DOCKERFILE_LOC=~/Projects/cybexp-privacy/
+DIR_PATH=`dirname $0`
+FULL_PATH=`readlink --canonicalize $DIR_PATH`
 
+DOCKERFILE_LOC=$FULL_PATH
 DOCKER_STATE=`sudo systemctl status docker | grep Active: | head -n 1 | awk '{print $2}'`
 
 if [ "$DOCKER_STATE" = "inactive" ]; then
@@ -9,7 +11,11 @@ if [ "$DOCKER_STATE" = "inactive" ]; then
    sudo systemctl start docker
    exec $0
 elif [ "$DOCKER_STATE" = "active" ]; then
-   docker build -t cybexp-collector $DOCKERFILE_LOC && docker run -it cybexp-collector /bin/bash
+   sudo docker build -t cybexp-collector $DOCKERFILE_LOC && sudo docker run -it cybexp-collector /bin/bash
+   DOCKER_ID=`docker ps --all | grep cybexp-collector | awk '{print $1}'`
+   echo "Removing container"
+   # sudo docker stop $DOCKER_ID
+   sudo docker rm $DOCKER_ID
 else
    echo 'Failed...'
    exit 1
