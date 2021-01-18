4e0f266eacbe5850a1cb72d86f55b960c4082419
Ignacio Astaburuaga <ignaciochg@hotmail.com>
2021-01-18 12:09:55 -0800

    add basic auth

--- priv-libs/libs/web_client.py
+++ priv-libs/libs/web_client.py
@@ -16,9 +16,13 @@ from ORE import ORESecretKey
 
 
 
-def get_de_key(kms_api_base, debug=False):
+def get_de_key(kms_api_base, auth=None, debug=False):
+   kw_auth = {}
+   if auth != None:
+      kw_auth["auth"] = auth
+
    try:
-      r = requests.get(url=kms_api_base+"/get/key/de")
+      r = requests.get(url=kms_api_base+"/get/key/de", **kw_auth)
       raw_key = r.content
 
       return raw_key
@@ -31,9 +35,13 @@ def get_de_key(kms_api_base, debug=False):
 
 
 
-def get_ore_key(kms_api_base, debug=False):
+def get_ore_key(kms_api_base, auth=None, debug=False):
+   kw_auth = {}
+   if auth != None:
+      kw_auth["auth"] = auth
+
    try:
-      r = requests.get(url=kms_api_base+"/get/key/ore")
+      r = requests.get(url=kms_api_base+"/get/key/ore", **kw_auth)
       raw_key = r.content
       key = ORESecretKey.from_raw_bytes(raw_key)
       return key
@@ -46,11 +54,14 @@ def get_ore_key(kms_api_base, debug=False):
 
 
 
-def get_cpabe_pub_key(kms_api_base, debug=False):
+def get_cpabe_pub_key(kms_api_base, auth=None, debug=False):
+   kw_auth = {}
+   if auth != None:
+      kw_auth["auth"] = auth
 
    try:
       bsw07 = CPABEAlg()
-      r = requests.get(url=kms_api_base+"/get/key/cpabe-pk")
+      r = requests.get(url=kms_api_base+"/get/key/cpabe-pk", **kw_auth)
       raw_key = r.content
       key = bsw07.deserialize_charm_obj(raw_key)
       return key
@@ -64,11 +75,15 @@ def get_cpabe_pub_key(kms_api_base, debug=False):
 
 
 
-def get_org_cpabe_secret(kms_api_base, name, debug=False):
+def get_org_cpabe_secret(kms_api_base, name, auth=None, debug=False):
+   kw_auth = {}
+   if auth != None:
+      kw_auth["auth"] = auth
+
    try:
       bsw07 = CPABEAlg()
       req_body = {"name": name}
-      r = requests.post(url=kms_api_base+"/get/key/cpabe-sk", json=req_body)
+      r = requests.post(url=kms_api_base+"/get/key/cpabe-sk", json=req_body, **kw_auth)
       raw_key = r.content
       key = bsw07.deserialize_charm_obj(raw_key)
       return key
@@ -80,14 +95,20 @@ def get_org_cpabe_secret(kms_api_base, name, debug=False):
       return None
 
 
-def post_enc_data(base_url,data, debug=False):  
+def post_enc_data(base_url,data, auth=None, debug=False):  
+   kw_auth = {}
+   if auth != None:
+      kw_auth["auth"] = auth
+
    try:
       if type(data["index"]) != list or len(data)<=0:
          return False
+
       ser_data = pickle.dumps(data)
       r = requests.post(url=base_url+"/add/enc-data",
                           data=ser_data,
-                          headers={'Content-Type': 'application/octet-stream'})
+                          headers={'Content-Type': 'application/octet-stream'}
+                          **kw_auth)
       
       if r.status_code >= 300:
          return False
@@ -100,7 +121,7 @@ def post_enc_data(base_url,data, debug=False):
          traceback.print_exc()
       return False
 
-def query_enc_data(base_url, enc_val, enc_left_epoch=None, enc_right_epoch=None, left_inclusive=None, right_inclusive=None, debug=False):
+def query_enc_data(base_url, enc_val, enc_left_epoch=None, enc_right_epoch=None, left_inclusive=None, right_inclusive=None, auth=None, debug=False):
    try:
       if type(enc_val) != bytes:
          return None
@@ -112,6 +133,10 @@ def query_enc_data(base_url, enc_val, enc_left_epoch=None, enc_right_epoch=None,
          return None
       if type(right_inclusive) != bool and right_inclusive != None:
          return None
+
+      kw_auth = {}
+      if auth != None:
+         kw_auth["auth"] = auth
 
       data = {"index": enc_val}
       if enc_left_epoch:
@@ -128,7 +153,8 @@ def query_enc_data(base_url, enc_val, enc_left_epoch=None, enc_right_epoch=None,
       # print(ser_data)
       r = requests.post(url=base_url+"/query",
                           data=ser_data,
-                          headers={'Content-Type': 'application/octet-stream'})
+                          headers={'Content-Type': 'application/octet-stream'},
+                          **kw_auth)
       
       if r.status_code >= 300:
          if debug:
@@ -144,9 +170,13 @@ def query_enc_data(base_url, enc_val, enc_left_epoch=None, enc_right_epoch=None,
       return None
 
    
-def list_all_attributes(kms_api_base):
+def list_all_attributes(kms_api_base, auth=None):
+   kw_auth = {}
+   if auth != None:
+      kw_auth["auth"] = auth
+
    try:
-      r = requests.get(url=kms_api_base+"/get/attributes")
+      r = requests.get(url=kms_api_base+"/get/attributes", **kw_auth)
       if r.status_code >= 300:
          return None
       return r.content
@@ -157,3 +187,11 @@ def list_all_attributes(kms_api_base):
       if debug:
          traceback.print_exc()
       return None
+
+
+
+def test_auth(api_base, auth):
+   r = requests.get(url=api_base+"/", auth=auth)
+   if r.status_code >= 300:
+      return False
+   return True
